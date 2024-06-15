d3b6ff6c74ec3a1a970913635168b6e3f8d65e9b
surajjayraman <suraj.jayraman@gmail.com>
2024-06-15 08:49:22 -0400

    Update home page font styles

--- first-app/resources/views/home.blade.php
+++ first-app/resources/views/home.blade.php
@@ -7,8 +7,13 @@
             <img src="https://www.freecodecamp.org/news/content/images/size/w2000/2024/06/img_12--1-.jpg" alt="logo" style="height: 50px; border-right: 1px solid #333333;" class="pr-3"
             class="rounded-circle">
         </div>
-        <div class="col-9">
-            <h1>free code!</h1>
+        <div class="col-9 pt-5">
+            <div><h1>free code!</h1></div>
+            <div class="d-flex">
+                <div class="pr-4"><strong>153</strong> posts</div>
+                <div class="pr-4"><strong>23k</strong> followers</div>
+                <div class="pr-4"><strong>212</strong> following</div>
+            </div>
         </div>
     </div>
 </div>
