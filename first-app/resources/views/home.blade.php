b3f2c74fbe6d5d4f2ac8752b366a4a6641e12b99
surajjayraman <suraj.jayraman@gmail.com>
2024-06-15 09:12:35 -0400

    Update home page layout

--- first-app/resources/views/home.blade.php
+++ first-app/resources/views/home.blade.php
@@ -4,16 +4,38 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="https://www.freecodecamp.org/news/content/images/size/w2000/2024/06/img_12--1-.jpg" alt="logo" style="height: 50px; border-right: 1px solid #333333;" class="pr-3"
-            class="rounded-circle">
+            <img src="https://www.freecodecamp.org/news/content/images/size/w2000/2024/06/img_12--1-.jpg" alt="logo"
+                style="height: 50px; border-right: 1px solid #333333;" class="pr-3" class="rounded-circle">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>free code!</h1></div>
+            <div>
+                <h1>free code!</h1>
+            </div>
             <div class="d-flex">
                 <div class="pr-4"><strong>153</strong> posts</div>
                 <div class="pr-4"><strong>23k</strong> followers</div>
                 <div class="pr-4"><strong>212</strong> following</div>
             </div>
+            <div class="pt-4 font-weight-bold">freecodecamp.org</div>
+            <div>
+                <p>we are a non-profit organization that is supported by donations. we're a community of millions of
+                    people who are helping each other learn to code by building projects.</p>
+            </div>
+            <div><a href="#">www.freecodecamp.org</a></div>
+        </div>
+    </div>
+    <div class="row pt-4">
+        <div class="col-4">
+            <img src="https://www.freecodecamp.org/news/content/images/size/w2000/2024/06/img_12--1-.jpg" alt="logo"
+                class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://www.freecodecamp.org/news/content/images/size/w2000/2024/06/img_12--1-.jpg" alt="logo"
+                class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://www.freecodecamp.org/news/content/images/size/w2000/2024/06/img_12--1-.jpg" alt="logo"
+                class="w-100">
         </div>
     </div>
 </div>
