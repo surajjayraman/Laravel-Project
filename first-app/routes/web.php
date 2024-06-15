9af71edd2d1d6276817d9c4f0369ad9df9ce9da9
surajjayraman <suraj.jayraman@gmail.com>
2024-06-15 14:27:40 -0400

    Update routes web.php

--- first-app/routes/web.php
+++ first-app/routes/web.php
@@ -19,6 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\ProfilesController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
 
 
