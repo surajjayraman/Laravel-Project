bb51b11d3a527645886be94d8a799aee37891984
surajjayraman <suraj.jayraman@gmail.com>
2024-06-18 18:35:07 -0400

    Add routes for posts create

--- first-app/routes/web.php
+++ first-app/routes/web.php
@@ -19,6 +19,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/p', 'PostsController@create');
+
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
 
