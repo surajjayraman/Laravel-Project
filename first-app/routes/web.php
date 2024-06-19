0ac0c0ad200d6b5761c1fc2e6626bd37dc2a2fc2
surajjayraman <suraj.jayraman@gmail.com>
2024-06-19 12:01:03 -0400

    Update view page for create posts

--- first-app/routes/web.php
+++ first-app/routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/p', 'PostsController@create');
+Route::get('/p', [App\Http\Controllers\PostsController::class, 'create']);
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
